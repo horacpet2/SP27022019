dbd563ae47be7ca76c88055201543b8df7454ece
petr <horacpet2@gmail.com>
2019-03-10 19:30:11 +0100

    add gui prototype project in java

---
+++ prototype/gui/src/gui/Gui.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+
+/**
+ *
+ * @author petr
+ */
+public class Gui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        main_window  window = new main_window();
+        window.setVisible(true);
+    }
+    
+}
--- prototype/gui_prototype/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-
-gcc main.c -o gui_prototype $(pkg-config --libs --cflags gtk+-3.0)
